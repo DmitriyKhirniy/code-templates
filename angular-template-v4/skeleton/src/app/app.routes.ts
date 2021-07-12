172f8e7a4dcf59f6f4cabc47d634830d183a6229
Dmytro Khirnyi <49021301+dkhir@users.noreply.github.com>
2021-07-12 09:58:07 +0300

    fixes

---
+++ angular-template-v4/skeleton/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+
+import { EmptyRouteComponent } from './modules/empty-route/empty-route.component';
+import { MainComponent } from './modules/main/main.component';
+
+export const ApplicationRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'main',
+  },
+  {
+    path: 'main',
+    component: MainComponent,
+  },
+  {%- if cookiecutter.graphql_client == "yes" -%}
+  {
+    path: 'lazy',
+    loadChildren: () => import('./modules/lazy-example/lazy-page.module')
+      .then((m) => m.LazyPageModule),
+  },
+{% endif %}
+  { path: '**', component: EmptyRouteComponent },
+];
